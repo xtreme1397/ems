2a48948a55249ab9b833ec8914369a8d41617689
Abhishek Kumar <abhishek.k@miqdigital.com>
2018-10-21 11:57:34 +0530

    initial commit

--- org/ems/com/system/config/mongoConfig.js
+++ org/ems/com/system/config/mongoConfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 module.exports.connect = function () {
     // connect to mlab database
     // make sure to replace my db string & creds with your own
-    mongoose.connect('mongodb://root:abhi%401992@ds115963.mlab.com:15963/xtreme',{ useNewUrlParser: true, useCreateIndex: true })
+    mongoose.connect('mongodb://<username>:<password>@<mongourl>/<dbname>',{ useNewUrlParser: true, useCreateIndex: true })
     mongoose.connection.once('open', (err) => {
         if(!err){
             console.log('conneted to database');
